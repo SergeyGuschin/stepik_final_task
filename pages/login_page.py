c78ceb14ef98ebbee3bbf5cab234e10a40285f26
SergeyGuschin <sergey.gushchin@jetruby.com>
2024-01-10 03:38:53 +0300

    Урок 4.2 Реализация LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        substring = "login"
+        assert substring in current_url, "The substring not found in URL"
+        assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login Form is not present"
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration Form is not present"
+        assert True
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import time
-from selenium.webdriver.common.by import By
+
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -9,11 +10,12 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
-    # time.sleep(3)
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    time.sleep(3)
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
